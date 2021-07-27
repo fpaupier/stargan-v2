0ef937484715c9211634b7ee79b34ee6fd696f66
fpaupier <francois.paupier@gmail.com>
2021-07-27 08:18:04 +0200

    rm: training options from main

--- main.py
+++ main.py
@@ -83,18 +83,8 @@ if __name__ == '__main__':
     # training arguments
     parser.add_argument('--resume_iter', type=int, default=0,
                         help='Iterations to resume training/testing')
-    parser.add_argument('--batch_size', type=int, default=8,
-                        help='Batch size for training')
     parser.add_argument('--val_batch_size', type=int, default=32,
                         help='Batch size for validation')
-    parser.add_argument('--beta1', type=float, default=0.0,
-                        help='Decay rate for 1st moment of Adam')
-    parser.add_argument('--beta2', type=float, default=0.99,
-                        help='Decay rate for 2nd moment of Adam')
-    parser.add_argument('--weight_decay', type=float, default=1e-4,
-                        help='Weight decay for optimizer')
-    parser.add_argument('--num_outs_per_domain', type=int, default=10,
-                        help='Number of generated images per domain during sampling')
 
     # misc
     parser.add_argument('--num_workers', type=int, default=4,
